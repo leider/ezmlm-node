29c9c85fb389b43de490c1eb7cc785828ada0c23
leider <derleider@web.de>
2015-02-07 19:03:56 +0100

    separating specs to have more granularity

---
+++ spec/generalCommandsSpec.js
@@ -0,0 +1,68 @@
+'use strict';
+
+var
+  sinon = require('sinon'),
+  proxyquire = require('proxyquire'),
+  expect = require('must'),
+  flagsFactory = require('../lib/flags');
+
+describe('top level functions', function () {
+  var
+    flags,
+    ezmlm,
+    execSpy;
+
+  beforeEach(function () {
+    flags = flagsFactory();
+    execSpy = sinon.spy();
+    ezmlm = proxyquire('../lib/commands', {'./ezmlmExec': {perform: execSpy}})('/fqHomedirectory', 'derleider.de');
+  });
+
+  describe('creating a list', function () {
+    it('performs a clean "make"', function () {
+      ezmlm.createListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.be('ezmlm-make -abDEfgHIJLMNOPRSTu /fqHomedirectory/ezmlm/someCrazyName /fqHomedirectory/.qmail-someCrazyName someCrazyName derleider.de');
+    });
+
+    it('performs a "make" with digests', function () {
+      flags.digest(true);
+      ezmlm.createListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.be('ezmlm-make -abdEfgHIJLMNOPRSTu /fqHomedirectory/ezmlm/someCrazyName /fqHomedirectory/.qmail-someCrazyName someCrazyName derleider.de');
+    });
+
+    it('performs a "make" with moderation', function () {
+      flags.moderate(true);
+      ezmlm.createListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.be('ezmlm-make -abDEfgHIJLmNOPRSTu /fqHomedirectory/ezmlm/someCrazyName /fqHomedirectory/.qmail-someCrazyName someCrazyName derleider.de');
+    });
+
+    it('never is in editmode', function () {
+      flags.edit(true);
+      ezmlm.createListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.match(/ezmlm-make -\w\w\wE/);
+    });
+  });
+
+  describe('editing a list', function () {
+    it('never has qmail-file, listname and domain as arguments', function () {
+      ezmlm.editListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.be('ezmlm-make -abDefgHIJLMNOPRSTu /fqHomedirectory/ezmlm/someCrazyName');
+    });
+
+    it('always is in editmode', function () {
+      flags.edit(false);
+      ezmlm.editListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.match(/ezmlm-make -\w\w\we/);
+    });
+
+    it('performs a "make" with moderation', function () {
+      flags.moderate(true);
+      ezmlm.editListNamed('someCrazyName', flags);
+      expect(execSpy.args[0][0]).to.be('ezmlm-make -abDefgHIJLmNOPRSTu /fqHomedirectory/ezmlm/someCrazyName');
+    });
+
+  });
+
+});
+
+
---
+++ spec/listingUsersSpec.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var
+  sinon = require('sinon'),
+  proxyquire = require('proxyquire'),
+  expect = require('must');
+
+describe('listing users', function () {
+  var
+    ezmlm,
+    execStub;
+
+  beforeEach(function () {
+    execStub = sinon.stub();
+    ezmlm = proxyquire('../lib/commands', {'./ezmlmExec': {perform: execStub}})('/fqHomedirectory', 'derleider.de');
+  });
+
+
+  it('creates the correct command string', function () {
+    ezmlm.usersOfList('someCrazyName');
+    expect(execStub.args[0][0]).to.be('ezmlm-list /fqHomedirectory/ezmlm/someCrazyName');
+  });
+
+  it('creates an arry from stdout', function (done) {
+    execStub.callsArgWith(1, null, 'someuser@domain.org\nsomeother@domain.org');
+    ezmlm.usersOfList('someCrazyName', function (error, users) {
+      expect(users).to.be.an.array();
+      expect(users).to.have.length(2);
+      expect(users[0]).to.be('someuser@domain.org');
+      expect(users[1]).to.be('someother@domain.org');
+      done();
+    });
+  });
+
+  it('handles error from cmd line', function (done) {
+    execStub.callsArgWith(1, new Error('mmmpp'));
+    ezmlm.usersOfList('someCrazyName', function (error, users) {
+      expect(error).to.exist();
+      expect(users).to.be.an.array();
+      expect(users).to.be.empty();
+      done();
+    });
+  });
+
+  it('handles empty result from cmd line', function (done) {
+    execStub.callsArgWith(1, null, '');
+    ezmlm.usersOfList('someCrazyName', function (error, users) {
+      expect(users).to.be.an.array();
+      expect(users).to.be.empty();
+      done(error);
+    });
+  });
+
+});
+
+
