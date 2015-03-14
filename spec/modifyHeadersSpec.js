f349758d4388b88b5e5be1c3f389d9d1723bce4a
leider <derleider@web.de>
2015-03-14 13:37:31 +0100

    can now write headeradd / headerremove to enable replyto: header for replying to list

---
+++ spec/modifyHeadersSpec.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var
+  sinon = require('sinon'),
+  proxyquire = require('proxyquire'),
+  expect = require('must');
+
+describe('Changing replyTo', function () {
+  var
+    modifyHeaders,
+    execStub;
+
+  beforeEach(function () {
+    execStub = sinon.stub();
+    modifyHeaders = proxyquire('../lib/modifyHeaders', {'./ezmlmExec': {perform: execStub}})('/fqHomedirectory', 'derleider.de');
+  });
+
+  it('creates the right commands for reply to list', function (done) {
+    execStub.callsArgWith(1, null);
+    modifyHeaders.replyToList('someCrazyName', done);
+    expect(execStub.args[0][0]).to.contain('"Sender: <<#l#>@<#h#>>\nPrecedence: bulk\nX-No-Archive: yes\nList-Post: <mailto:<#l#>@<#h#>>');
+    expect(execStub.args[0][0]).to.contain('\nReply-To: <<#l#>@<#h#>>\n');
+    expect(execStub.args[0][0]).to.contain('" > /fqHomedirectory/ezmlm/someCrazyName/headeradd');
+    expect(execStub.args[1][0]).to.contain('"return-path\nreturn-receipt-to\ncontent-length\nprecedence\nx-confirm-reading-to\nx-pmrqc\nlist-subscribe\nlist-unsubscribe\nlist-help\nlist-post\nsender\n');
+    expect(execStub.args[1][0]).to.contain('reply-to\n');
+    expect(execStub.args[1][0]).to.contain('\" > /fqHomedirectory/ezmlm/someCrazyName/headerremove');
+  });
+
+  it('creates the right commands for reply to sender', function (done) {
+    execStub.callsArgWith(1, null);
+    modifyHeaders.replyToSender('someCrazyName', done);
+    expect(execStub.args[0][0]).to.contain('"Sender: <<#l#>@<#h#>>\nPrecedence: bulk\nX-No-Archive: yes\nList-Post: <mailto:<#l#>@<#h#>>');
+    expect(execStub.args[0][0]).to.not.contain('\nReply-To: <<#l#>@<#h#>>\n');
+    expect(execStub.args[0][0]).to.contain('" > /fqHomedirectory/ezmlm/someCrazyName/headeradd');
+    expect(execStub.args[1][0]).to.contain('"return-path\nreturn-receipt-to\ncontent-length\nprecedence\nx-confirm-reading-to\nx-pmrqc\nlist-subscribe\nlist-unsubscribe\nlist-help\nlist-post\nsender\n');
+    expect(execStub.args[1][0]).to.not.contain('reply-to\n');
+    expect(execStub.args[1][0]).to.contain('\" > /fqHomedirectory/ezmlm/someCrazyName/headerremove');
+  });
+
+});
+
+
